fa0889165f5caf5306c1d2769e4ec63cb7f4a043
llxxs <fly51997@qq.com>
2017-10-25 23:38:29 +0800

    add some slave

--- awada.py
+++ awada.py
@@ -9,10 +9,15 @@ import select
 def usage():
     print('awada portftd')
     print('-h; help')
+    print('-v; verbose')
     print('-listen portA,portB; listen two ports and transmit data')
-    print('-tran localport,target,targetport; listen a local port and transmit data to target:targetport')
+    print('-tran localport,targetip,targetport; listen a local port and transmit data to target:targetport')
+    print('-slave reverseip,reverseport,targetip,targetport; connect reverseip:reverseport with targetip:targetport')
 
 def subTransmit(recvier,sender,stopflag):
+    if '-v' in sys.argv:
+        verbose = True
+    isReleased = False
     while not stopflag['flag']:
         data = b""
         try:
@@ -21,9 +26,14 @@ def subTransmit(recvier,sender,stopflag):
                 if len(data) == 0:
                     time.sleep(0.1) #select加sleep为了多平台都可用
                     continue
-            print("Recv from ",recvier[1])
+            if 'lock' in stopflag and not isReleased:
+                stopflag['lock'].set()
+                isReleased = True
             sender[0].send(data)
-            print("Send to ",sender[1])
+            bytes = len(data)
+            if verbose:
+                print("Recv from ",recvier[1],"%db" % bytes)
+                print("Send to ",sender[1],"%db" % bytes)
         except Exception as e:
             stopflag['flag'] = True
             try:
@@ -32,14 +42,16 @@ def subTransmit(recvier,sender,stopflag):
             except:
                 pass
 
-def transmit(conns):
+def transmit(conns,lock=None):
     stopFlag = {'flag':False}
+    if lock is not None:
+        stopFlag['lock'] = lock
     connA, addressA, connB, addressB = conns
     threading.Thread(target=subTransmit,args=((connA,addressA),(connB,addressB), stopFlag)).start()
     threading.Thread(target=subTransmit, args=((connB, addressB), (connA, addressA), stopFlag)).start()
     while not stopFlag['flag']:
         time.sleep(3)
-    print("Connection closed.",addressA,addressB)
+    print("Connection closed.",addressA,"---",addressB)
 
 def bindToBind(portA,portB):
     socketA = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -113,7 +125,60 @@ def bindToConn(port,target,targetPort):
             connA.close()
             exit()
 
+
+
+def connToConn(reverseIp,reversePort,targetIp,targetPort):
+    continueFlag = False
+    while True:
+        data = b""
+        reverseSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        targetSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        reverseAddress = (reverseIp,reversePort)
+        targetAddress = (targetIp,targetPort)
+
+        try:
+            print("Connect ot ",reverseAddress)
+            reverseSocket.connect(reverseAddress)
+            print("Connect ok!")
+        except:
+            print("Connect failed!")
+            exit()
+        while True:
+            try:
+                if select.select([reverseSocket],[],[]) == ([reverseSocket],[],[]):
+                    data = reverseSocket.recv(20480)
+                    if len(data) == 0:
+                        time.sleep(0.1)
+                        continue
+                    else:
+                        break
+                time.sleep(0.1)
+            except:
+                continueFlag = True
+        if continueFlag == True:
+            continueFlag = False
+            continue
+
+        while True:
+            try:
+                print("Connect ot ",targetAddress)
+                targetSocket.connect(targetAddress)
+                targetSocket.send(data)
+                print("Connect ok!")
+            except:
+                print("TargetPort is not open")
+                reverseSocket.close()
+                continueFlag = True
+                break
+        if continueFlag == True:
+            continueFlag = False
+            continue
+
+        multiprocessing.Process(target=transmit,args=((reverseSocket,reverseAddress,targetSocket,targetAddress))).start()
+
+
 def main():
+    global verbose
     if '-h' in sys.argv:
         usage()
         exit()
@@ -136,6 +201,17 @@ def main():
             targetPort = int(sys.argv[index+3])
             assert port!=0 and targetPort!=0
             bindToConn(port,target,targetPort)
+        except:
+            print("Something wrong")
+        exit()
+    elif '-slave' in sys.argv:
+        index = sys.argv.index('-slave')
+        try:
+            reverseIp = sys.argv[index+1]
+            reversePort = int(sys.argv[index+2])
+            targetIp = sys.argv[index+3]
+            targetPort = int(sys.argv[index+4])
+            connToConn(reverseIp,reversePort,targetIp,targetPort)
         except:
             print("Something wrong")
         exit()
